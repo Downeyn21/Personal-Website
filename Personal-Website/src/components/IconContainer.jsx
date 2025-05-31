95296d17e81e79e2fb3314d534ad067e795c2bf9
Nathaniel Downey <downeyn21@gmail.com>
2025-05-30 20:11:46 -0400

    added icons

--- Personal-Website/src/components/IconContainer.jsx
+++ Personal-Website/src/components/IconContainer.jsx
@@ -3,15 +3,29 @@ function IconContainer() {
     for(let i = 0; i < 50; i++) {
         icons.push(i + 1)
     }
-    
-    console.log(icons)
 
     return ( 
         <>
             <div className="icon-container">
-                {icons.map((num, index) => (
+                {/* {icons.map((num, index) => (
                     <div key={index} className="icon">{num}</div>
-                ))}
+                ))} */}
+                <div className="icon">
+                    <img src="/img/folder-icon.png" alt="folder" />
+                    <span>Projects</span>
+                </div>
+                <div className="icon">
+                    <img src="/img/resume-icon.png" alt="resume" />
+                    <span>Resume</span>
+                </div>
+                <div className="icon">
+                    <img src="/img/User-info.png" alt="about" />
+                    <span>About</span>
+                </div>
+                <div className="icon">
+                    <img src="/img/music-player.png" alt="music-player" />
+                    <span>Music</span>
+                </div>
             </div>
         </>
      );
